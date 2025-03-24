ee24242bb60e74826f2195add441fc254d6487a1
rosisneupane <rosisneupane1@gmail.com>
2025-03-24 14:09:50 +0545

    test:test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Hello, FastAPI! testing"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
