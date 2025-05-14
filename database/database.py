09522db02f82be336876ba70388011ca4076c4ea
rosisneupane <rosisneupane1@gmail.com>
2025-05-14 13:52:10 +0545

    feat:db url

--- database/database.py
+++ database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #DATABASE_URL = "sqlite:///./users.db"  # SQLite DB file
-DATABASE_URL = "postgresql://rosis_db_user:t52mT7O5m7hrJp8QtStPH9V6ITVUlu3W@dpg-d0i41l6mcj7s739m8rh0-a.oregon-postgres.render.com/rosis_db"
+DATABASE_URL = "postgresql://rosis_database_user:n6fjcRQAMnfGHZpT3jGkmruhR3OF2rnC@dpg-d0i4t956ubrc73d62lm0-a.oregon-postgres.render.com/rosis_database"
 
 engine = create_engine(DATABASE_URL)
 
